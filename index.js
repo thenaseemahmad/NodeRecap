47cbd328be399db562b324e2ae36d2b690454159
thenaseemahmad <naseemahmad244@gmail.com>
2023-10-24 15:48:44 +0530

    Corrected the sample api

--- index.js
+++ index.js
@@ -16,24 +16,27 @@ app.get('/',(req, res)=> {
     res.sendFile(__dirname+"/public/index.html");
 });
 
+//this is how to call an API via node http inbuild pack
 app.get("/apitest",function(req,res){
     //here is the const that will contain the api detail
     const options = 
     {
-        hostname:"bored-api.appbrewery.com",
-        path: "/random",
+        hostname:"jsonplaceholder.typicode.com",
+        path: "/posts",
         method:"GET",
         headers:{"Content-Type":"application/json"}
     };
 
     //here to make request 
     const  request = http.request(options,function(response){
+        response.setEncoding('utf8');
         let data = "";
         response.on("data",(chunk)=>{data +=chunk;})
         response.on("end",()=>{
             try{
-                //const result = JSON.parse(data);
-                console.log(data)
+                const result = JSON.parse(data);
+                //console.log(data);
+                res.send(result);
 
             }
             catch(erro){
