1092a8c1f2e90849517ce60151e7a823d9fa8dde
thenaseemahmad <naseemahmad244@gmail.com>
2023-10-29 16:42:57 +0530

    First Mongodb commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -11,21 +12,31 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan('combined'));
 const port = 3000;
 
+mongoose.connect("mongodb+srv://japsinnaseem:FyEIWWloif0nDvUG@cluster0.c9xlloe.mongodb.net/userDB?retryWrites=true&w=majority",{useNewUrlParser:true});
+
+const userSchema = new mongoose.Schema({username:String,password:String});
+const User = new mongoose.model('users',userSchema);
+
 app.get('/',(req, res)=> {
-    res.sendFile(__dirname+"/public/index.html");
+    //res.sendFile(__dirname+"/public/index.html");
+    const newUser = new User({
+        username:'naseem@gmail.com',
+        password:'yeterayemera'
+    })
+    // newUser.save(function(err){
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     else{
+    //         res.send('User added successfully');
+    //     }
+    // })
+    newUser.save().then(function(){
+        console.log("user added successfully");
+    })
 });
 
-app.post('/createfile',(req,res)=>{
-    //console.log(req.body)
-    fs.writeFile(req.body.FileName,req.body.FileText,(err)=>{
-        if(err){
-            console.log("internal error");
-        }
-        else{
-            res.sendStatus(201);
-        }
-    });    
-});
+
 
 app.listen(port, ()=> {
     console.log('Server is running on 3000');
